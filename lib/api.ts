d0bcc9a7f84f97b63e5156d82e7cf40199cd6600
Nuno Mendes <fnuno91@gmail.com>
2025-03-03 23:26:38 +0000

    Refactor API configuration to use production URL

--- lib/api.ts
+++ lib/api.ts
@@ -10,6 +10,8 @@ import {
   ReturnedUserOrder,
 } from "@/lib/types";
 
+//Testing Production API
+
 // const API_BASE_URL = "http://localhost:5000/api"; // URL da API
 const API_BASE_URL = "https://libproject-api.onrender.com/api";
 
